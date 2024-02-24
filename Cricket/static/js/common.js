9df31c1fe2b0b1f06d217b468d0c7f07dc7aa69f
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-02-24 17:42:50 +0545

    static website

---
+++ Cricket/static/js/common.js
@@ -0,0 +1,13 @@
+// Dynamically load the content of nav.html into the container
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarContainer = document.getElementById('dynamicNavbarContainer');
+
+    fetch('/Section/top_nav.html')
+        .then(response => response.text())
+        .then(data => {
+            navbarContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading nav.html:', error);
+        });
+});
